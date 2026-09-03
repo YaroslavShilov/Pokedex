cf1e7f33ab13236cf33572e1b5c5b694bcc3aa18
Yaroslav Shilov <workyarik@gmail.com>
2026-09-03 14:47:15 +0400

    add gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 // import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { routes } from "./routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
-  <BrowserRouter>{routes}</BrowserRouter>,
+  <HashRouter>{routes}</HashRouter>,
   // </StrictMode>,
 );
